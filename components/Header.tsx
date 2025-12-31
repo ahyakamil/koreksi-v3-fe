e8ba603e06bf9820d841ed0d594753b060b1d050
aktechid <aktechid@gmail.com>
2025-12-31 08:37:19 +0700

    trending

--- components/Header.tsx
+++ components/Header.tsx
@@ -68,7 +68,7 @@ export function Header() {
               <Search className="absolute left-3 top-1/2 -translate-y-1/2 w-4 h-4 text-gray-400" />
               <input
                 type="text"
-                placeholder="Search Social Journal"
+                placeholder="Search"
                 className="pl-10 pr-4 py-2 bg-gray-100 rounded-full w-60 focus:outline-none focus:ring-2 focus:ring-blue-500"
               />
             </div>
