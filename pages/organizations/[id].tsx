7248908c842385baf64ff7ec290c36b3d3b61415
aktechid <aktechid@gmail.com>
2026-01-03 22:25:26 +0700

    news

--- pages/organizations/[id].tsx
+++ pages/organizations/[id].tsx
@@ -684,12 +684,16 @@ const OrganizationDetailsPage: React.FC = () => {
                   <Heart className="w-5 h-5" />
                 </button>
               )}
-              <button
-                onClick={() => router.push(`/organizations/${id}/news`)}
-                className="py-2 px-1 border-b-2 font-medium text-sm border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300"
-              >
-                <Pencil className="w-5 h-5" />
-              </button>
+              {
+                canManage && (
+                  <button
+                    onClick={() => router.push(`/organizations/${id}/news`)}
+                    className="py-2 px-1 border-b-2 font-medium text-sm border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300"
+                  >
+                    <Pencil className="w-5 h-5" />
+                  </button>
+                )
+              }
             </nav>
           </div>
         </div>
